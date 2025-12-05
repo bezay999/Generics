68046258cb4c8de0fe7f4394ab12a0acc10975ac
Vladyslav Bezai <vbezai@MacBook-Air-Vladyslav.local>
2025-12-05 14:39:24 +0100

    Initial commit

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class InventoryManagement
+{
+    public static void Main(string[] args)
+    {
+        List<string> inventoryList = new List<string>();
+        
+        inventoryList.Add("Printer");
+        inventoryList.Add("Laptop");
+        inventoryList.Add("Desk Chair");
+        inventoryList.Add("Monitor");
+        inventoryList.Add("Keyboard");
+        
+        Console.WriteLine(inventoryList.Count);
+        
+        bool hasDeskChair = inventoryList.Contains("Desk Chair");
+        Console.WriteLine(hasDeskChair);
+        
+        bool removed = inventoryList.Remove("Printer");
+        Console.WriteLine(removed);
+
+        foreach (var item in inventoryList)
+        {
+            Console.WriteLine(item);
+        }
+
+        List<string> newItems = new List<string>{"Mouse", "Desk Lamp"};
+        
+        inventoryList.AddRange(newItems);
+        inventoryList.Remove("Mouse");
+        inventoryList.Remove("Desk Lamp");
+
+        List<string> topInventory = inventoryList.GetRange(0, 3);
+        
+        Console.WriteLine(topInventory.Count);
+        
+        foreach (var item in topInventory)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
